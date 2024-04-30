a3346a4d1769d04e2488cefd32be390816d2bea5
Tyler Browne <44354282+tbrowne5@users.noreply.github.com>
2024-04-29 21:49:21 -0400

    Major update to streamlit_app.py
    Improved functionality, added model selection sidebar, added citations, changed plot colour and background, etc.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import gaussian_kde
 
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 def load_model():
     model = tf.keras.models.load_model('Citro_TevSpCas9.h5')
     return model
@@ -77,24 +84,13 @@ def output_download(df):
     )
 
 def generate_plot(preds):
-    data = np.array(preds) #np.random.normal(0, 1, size=100)
-    #data = np.sort(data)
-    #data = data[::-1]
+    data = np.array(preds)
     st.write(np.max(data))
-
-    # Create a histogram using Matplotlib
-    #fig, ax = plt.subplots()
-    #ax.hist(data, bins=20, alpha=0.5, color='blue')
-    #ax.set_title('Histogram (Matplotlib)')
-    #ax.set_xlabel('Values')
-    #ax.set_ylabel('Frequency')
-
-    # Display the plot
-    #st.pyplot(fig)
 
     # Create a density plot using Seaborn
     fig2, ax2 = plt.subplots()
-    backgroundalpha = 0.3
+    
+    backgroundalpha = 1.0
     ax2.axvspan(-3.5, -2.5, color='#D9DD17', alpha=backgroundalpha)    # Coloring the background from -3 to -2
     ax2.axvspan(-2.5, -1.5, color='#A0D52F', alpha=backgroundalpha) # Coloring the background from -2 to -1
     ax2.axvspan(-1.5, -0.5, color='#70C94B', alpha=backgroundalpha)  # Coloring the background from -1 to 0
@@ -102,6 +98,8 @@ def generate_plot(preds):
     ax2.axvspan(0.5, 1.5, color='#2DA86F', alpha=backgroundalpha)    # Coloring the background from 0 to 1
     ax2.axvspan(1.5, 2.5, color='#1D9677', alpha=backgroundalpha)     # Coloring the background from 1 to 2
     ax2.axvspan(2.5, 3.5, color='#1B847D', alpha=backgroundalpha)   # Coloring the background from 2 to 3
+    
+    #sns.set(rc={'axes.facecolor':'white', 'figure.facecolor':'white'})
     sns.histplot(data, kde=True, color='black', ax=ax2)
     labels = ["Rare","Very Low","Low","Moderate","High","Very High","Rare"]
     for i in range(0,7):
@@ -117,12 +115,111 @@ def generate_plot(preds):
     ax2.set_title('How to interpret predicted scores')
     ax2.set_xlabel('Predicted activity scores')
     ax2.set_ylabel('Density')
+    
+    #params = {"ytick.color" : "w",
+    #      "xtick.color" : "w",
+    #      "axes.labelcolor" : "w",
+    #      "axes.edgecolor" : "w"}
+    #plt.rcParams.update(params)
+    
+    ax2.xaxis.label.set_color('white')
+    ax2.yaxis.label.set_color('white')
+    ax2.title.set_color('white')
+
+    # Change the color of all spine elements on the graph
+    for spine in ax2.spines.values():
+        spine.set_color('white')
+
+    # Set tick colors
+    ax2.tick_params(axis='x', colors='white')
+    ax2.tick_params(axis='y', colors='white')
+
+    # Change the legend text to white
+    legend = ax2.get_legend()
+    for text in legend.get_texts():
+        text.set_color('white')
+    
+    plt.savefig("seaborn_plot.png", bbox_inches='tight', transparent=True)
+    plt.close()
 
     # Display the plot
-    st.pyplot(fig2)
+    col1.image("seaborn_plot.png", use_column_width=True)
+    #st.pyplot(fig2)
 
-st.title('crisprHAL: bacterial Cas9/sgRNA activity prediction')
+st.title('crisprHAL')
+st.subheader('A generalizable bacterial Cas9/sgRNA prediction model')
 uploaded_file = st.file_uploader("Upload a FASTA file", type=["fasta","txt"])
+
+option = st.sidebar.selectbox(
+    'Select the nuclease:',
+    ('TevSpCas9', 'SpCas9', 'eSpCas9')
+)
+
+crisprHALtevspcas9info = """
+Model:
+<br>• Name: crisprHAL, TevSpCas9
+<br>• Paper: https://www.nature.com/articles/s41467-023-41143-7
+<br>• Source Code: [Github/tbrowne5/crisprHAL](https://github.com/tbrowne5/crisprHAL)
+"""
+crisprHALsource = """
+Source Code: [Github/tbrowne5/crisprHAL](https://github.com/tbrowne5/crisprHAL)
+"""
+guoespcas9datasource = """
+Base Model Training Data: <br>• <i>[Escherichia coli, eSpCas9](https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA450978)</i>
+"""
+
+crisprHALtevspcas9datasource = """
+Transfer Learning Training Data:<br>• <i>[Escherichia coli, TevSpCas9](https://www.ncbi.nlm.nih.gov/bioproject/PRJNA939699)</i>
+"""
+
+#st.sidebar.write('You selected:', option)
+if option == 'TevSpCas9':
+    #st.sidebar.write('You have selected:', option)
+    st.sidebar.title('TevSpCas9 Model')
+    st.sidebar.write('Enzyme: TevSpCas9')
+    st.sidebar.write('Model: crisprHAL')
+    st.sidebar.markdown(crisprHALsource, unsafe_allow_html=True)
+    #st.sidebar.markdown(crisprHALtevspcas9info, unsafe_allow_html=True)
+    #st.sidebar.write('• Training Data: E. coli')
+    st.sidebar.markdown(guoespcas9datasource, unsafe_allow_html=True)
+    st.sidebar.markdown(crisprHALtevspcas9datasource, unsafe_allow_html=True)
+    #st.sidebar.markdown(crisprHALcitation, unsafe_allow_html=True)
+footer = """
+<style>
+.footer {
+    color: white;                 /* White text color */
+    position: fixed;              /* Fixed position to stay at the bottom */
+    left: 2px;                      /* Align to the left edge */
+    bottom: 0px;                    /* Align to the bottom edge */
+    height: 80px;                 /* Footer height */
+    /*width: calc(100% - 260px);*/    /* Footer width accommodating the sidebar */
+    width: 100%;                 /* Footer width */
+    background-color: #00000D;      /* Colour for background */
+    text-align: left;          /* Align text inside the footer to the right */
+    padding-left: 350px;        /* Left padding for the text */
+    padding-right: 25px;        /* Right padding for the text */
+    font-size: 1em;
+    line-height: 25px;            /* Center text vertically */
+    overflow: hidden;
+}
+
+@media (max-width: 765px) {
+    .footer {
+        padding-left: 25px;        /* Reduce left padding for smaller screens or when sidebar is likely hidden */
+    }
+}
+
+</style>
+<div class="footer">
+    <div class="footer-content">
+        <b>CITATION:</b><br>Ham, D.T., Browne, T.S., Banglorewala, P.N. et al. A generalizable Cas9/sgRNA prediction model using machine transfer learning with small high-quality datasets. <em>Nat Commun</em> <b>14</b>, 5514 (2023). <a>doi.org/10.1038/s41467-023-41143-7</a>
+    </div>
+</div>
+"""
+# CITATION:
+#Ham, D.T., Browne, T.S., Banglorewala, P.N. et al. A generalizable Cas9/sgRNA prediction model using machine transfer learning with small high-quality datasets. <em>Nat Commun</em> <b>14</b>, 5514 (2023). [https://doi.org/10.1038/s41467-023-41143-7](https://doi.org/10.1038/s41467-023-41143-7)</p>
+
+st.markdown(footer, unsafe_allow_html=True)
 
 if uploaded_file is not None:
     fasta_content = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
@@ -141,14 +238,20 @@ if uploaded_file is not None:
         #st.write(outputdf)
         
         output_download(outputdf)
+
+        col1, col2 = st.columns([3,2])
+
         generate_plot(predictions)
         
-        st.write("Best guides found:")
+        col2.subheader("Best guides found")
         top=1
         for item in output:
-            st.write("#" + str(top) + " sgRNA: " + str(item[0:20]) + ", score: " + str(output[item]))
+            score = str(output[item])
+            if len(score) > 6: score = score[0:5]
+            col2.write("#" + str(top) + ": " + str(item[0:20]) + " " + score) #str(output[item]))
             top+=1
             if top > 5: break
+        col2.write("Found " + str(len(output)) + " sgRNA target sites.")
         #st.write("\n\n\n\n\nALL OUTPUTS:")
         #for item in output:
         #    st.write(" sgRNA: " + str(item[0:20]) + ", score: " + str(output[item]))
